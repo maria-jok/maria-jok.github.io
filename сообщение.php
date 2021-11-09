c337eedce2cacc1a6995eb94ccb857c7201b5bd7
maria-jok <92333160+maria-jok@users.noreply.github.com>
2021-11-09 17:37:24 +0200

    Create сообщение.php

---
+++ сообщение.php
@@ -0,0 +1,38 @@
+<html>
+<body>
+
+
+
+
+
+
+<p>Ваш пароль: <?php echo htmlspecialchars($_POST["сообщение"]); ?></p>
+
+
+<?php
+date_default_timezone_set("Europe/Moscow");
+
+$новыйряд = date("l jS \of F Y h:i:s A") . "(MSK/UTC+3)         Пароль: " . htmlspecialchars($_POST["сообщение"]) . "\n";
+
+file_put_contents("пароли.txt", $новыйряд, FILE_APPEND)
+
+
+?>
+
+
+
+
+<a href="/"> Назад </a>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
