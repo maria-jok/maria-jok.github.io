493fc91470194236d06ec17b57d4ca88a043b7f3
maria-jok <92333160+maria-jok@users.noreply.github.com>
2021-11-09 17:39:53 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,60 @@
+
+@mariajok
+
+
+Create Repl
+
+Upgrade
+Home
+Apps
+BETA
+My Repls
+Talk
+Learn
+Teams
+Curriculum
+Blog
+About
+Careers
+Pricing
+Discord
+
+
+mariajok
+mariajok
+/
+LawfulAdeptFacts
+PHP Web Server
+
+Stop
+
+Invite
+
+Files
+
+
+
+1234567891011121314151617181920212223242526272829
+<?php
+
+$сообщение ="/";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  $сообщение = htmlspecialchars($_POST["сообщение"]);
+}
+
+?>
+
+
+
+https://LawfulAdeptFacts.mariajok.repl.co
+
+
+
+
+
+Console
+Shell
+Markdown
+
+LawfulAdeptFacts - Replit
